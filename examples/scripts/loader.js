99f86799962ba66c07c52b39bb45f298d1e293f0
Gerald Yeo <gerald@fusedthought.com>
2015-05-16 16:52:07 +0800

    some comments in example

--- examples/scripts/loader.js
+++ examples/scripts/loader.js
@@ -1,19 +1,27 @@
-/*jslint browser:true */
+/* 
+  Isomorphic React renders twice. Once on the HTML page, once in JavaScript.
+  If the props are the same on HTML and JavaScript, the template is synced,
+  allowing subsequent updates without destroying user input:
 
-// Isomorphic React renders twice. Once on the HTML page, once in JavaScript.
-// If the props are the same on HTML and JavaScript, the template is synced,
-// allowing subsequent updates without destroying user input:
-// Modified from: http://www.crmarsh.com/react-ssr/
+  Modified from: http://www.crmarsh.com/react-ssr/
+*/
 
 import EntryTemplate from '../templates/entry.jsx';
 import React from 'react';
 
-// templates/entry.jsx populates this value
+
+
+/*
+  In an actual environment, this should be
+  retrieved from a Store or some other database function.
+*/
 const props = JSON.parse(document.getElementById('props').innerHTML);
+
+
+
 
 // Render
 let Entry = React.createFactory(EntryTemplate);
-
 
 React.render(
   new Entry(props),
