173bc9feb9af963e96ec0b0bcab5979bae4da0bd
Gerald Yeo <gerald@fusedthought.com>
2017-04-22 23:07:15 +0800

    added more tests

---
+++ tests/jsx-render-engine/core/registerExtensions.spec.js
@@ -0,0 +1,25 @@
+import {expect} from 'chai';
+import {spy} from 'sinon';
+import registerExtensions from 'src/jsx-render-engine/core/registerExtensions';
+
+describe('jsx-render-engine/core/registerExtensions', function () {
+  it('should register babel and ignored extensions', function () {
+    const registerExtensionWithTransformer = spy();
+    const registerExtensionToIgnore = spy();
+    registerExtensions.__Rewire__('registerExtensionWithTransformer', registerExtensionWithTransformer);
+    registerExtensions.__Rewire__('registerExtensionToIgnore', registerExtensionToIgnore);
+
+    const options = {
+      requireTemplateExt: ['.jsx', 'jsx2'],
+      requireIgnoreExt: ['.t1', '.t2'],
+    }
+
+    registerExtensions(options);
+
+    registerExtensions.__ResetDependency__('registerExtensionWithTransformer');
+    registerExtensions.__ResetDependency__('registerExtensionToIgnore');
+
+    expect(registerExtensionWithTransformer.calledTwice).to.be.true;
+    expect(registerExtensionToIgnore.calledTwice).to.be.true;
+  });
+});
